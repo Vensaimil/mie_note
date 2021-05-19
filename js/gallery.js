8c2fc5c5a6c088d3bffc246408683e1024d364fe
Dinara <rakhimgaliyevna@gmail.com>
2021-05-19 21:47:10 +0600

    21

---
+++ js/gallery.js
@@ -0,0 +1,35 @@
+let galleryPicAll = document.getElementById("galleryAll");
+let galleryPic1 = document.getElementById("gallery1pic");
+let galleryPic2 = document.getElementById("gallery2pic");
+let galleryPic3 = document.getElementById("gallery3pic");
+let galleryPic4 = document.getElementById("gallery4pic");
+let galleryPic5 = document.getElementById("gallery5pic");
+let galleryPic6 = document.getElementById("gallery6pic");
+let galleryPic7 = document.getElementById("gallery7pic");
+let galleryPic8 = document.getElementById("gallery8pic");
+
+galleryPic1 = addEventListener("click", function(){
+    document.body.style.background = 'url(https://artdoart.com/uploads/news/4441_d4ca5a8239e8c49b9.jpg)';
+});
+galleryPic2 = addEventListener("click", function(){
+    document.body.style.background = 'url(https://paintingplanet.ru/pictures/image1072_1.jpg)';
+});
+galleryPic3 = addEventListener("click", function(){
+    document.body.style.background = 'url(https://3dnews.ru/assets/external/illustrations/2014/07/05/823437/mone_big.jpg)';
+});
+galleryPic4 = addEventListener("click", function(){
+    document.body.style.background = 'url(https://cs6.pikabu.ru/post_img/big/2014/04/07/11/1396897063_1421524292.jpg)';
+});
+galleryPic5 = addEventListener("click", function(){
+    document.body.style.background = 'url(https://avatars.mds.yandex.net/get-zen_doc/1945572/pub_5d4ad10fa1b4f100ae7d37b5_5d4ad2443f548700ad47c24d/scale_1200)';
+});
+galleryPic6 = addEventListener("click", function(){
+    document.body.style.background = 'url(http://cs624124.vk.me/v624124255/274a/a7r5HdYwOeU.jpg)';
+});
+galleryPic7 = addEventListener("click", function(){
+    document.body.style.background = 'url(https://dynamo.kiev.ua/media/swfupload/2020/09/krik-munka.jpeg)';
+});
+galleryPic8 = addEventListener("click", function(){
+    document.body.style.background = 'url(https://art-holst.com.ua/wp-content/uploads/kartiny-impressionizm-6.jpg)';
+});
+
